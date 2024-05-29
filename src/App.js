2dacfb9e3d4552176b4a232b3a296293c0755386
isurudilshan101 <isuru.dilshan101@gmail.com>
2024-05-29 15:33:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="">
+      <div className="home">
+        <Home />
+        {/* <section/> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
